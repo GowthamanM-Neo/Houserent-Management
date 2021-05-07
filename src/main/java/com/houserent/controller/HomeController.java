c7fdb05e15d9926ebbf0eb6050abad0346a7b69a
GowthamanM <gowthamancode@gmail.com>
2021-05-07 11:48:34 +0530

    Last Commit

--- src/main/java/com/houserent/controller/HomeController.java
+++ src/main/java/com/houserent/controller/HomeController.java
@@ -7,29 +7,37 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.houserent.model.*;
 import com.houserent.repo.*;
 
 
-@Controller
+@CrossOrigin(origins = "http://localhost:4200/")
+@RestController
 public class HomeController {
 
 	@Autowired
 	RoleRepo RoleRepo;
 	@Autowired
 	UserRepo UserRepo;
+//	@GetMapping("/")
+//	public String homePage() {
+//		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//		boolean hasBuyerRole = authentication.getAuthorities().stream()
+//		          .anyMatch(r -> r.getAuthority().toString().equals("ROLE_BUYER"));
+//		System.out.println(authentication.getAuthorities().stream().collect(Collectors.toList()));
+//		if(hasBuyerRole) {
+//			return "redirect:/signup";
+//		}else {
+//			return "redirect:/login";
+//		}
+//	}
+	
 	@GetMapping("/")
-	public String homePage() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		boolean hasBuyerRole = authentication.getAuthorities().stream()
-		          .anyMatch(r -> r.getAuthority().toString().equals("ROLE_BUYER"));
-		System.out.println(authentication.getAuthorities().stream().collect(Collectors.toList()));
-		if(hasBuyerRole) {
-			return "redirect:/signup";
-		}else {
-			return "redirect:/login";
-		}
+	public String send() {
+		return "data from Spring";
 	}
 }
